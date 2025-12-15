2a09d0882b2d209ea842a121502d34b5e77881c2
Daniil Ryzhenko <ryzhenko62@gmail.com>
2025-12-15 20:41:27 +0500

    Добавьте файлы проекта.

---
+++ BookStore.Core/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Core
+{
+    public class Book
+    {
+        private Book(Guid id, string title, string description, decimal price)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+            Price = price;
+        }
+        public Guid Id { get; }
+        public string Title { get; } = string.Empty;
+        public string Description { get; } = string.Empty;
+        public decimal Price { get; }
+    }
+}
